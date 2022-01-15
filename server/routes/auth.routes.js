8d52c301d301ea0e61577923a5dabfd2e378794b
Fer Ortega <fernando.ortega79@gmail.com>
2022-01-15 19:56:41 +0100

    Sign up JWT done

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -5,6 +5,10 @@ const User      = require('./../models/User.model')
 const router = express.Router()
 const saltRounds = 10
 
+
+
+//Sign up Route
+
 router.post('/signup', (req, res) => {
     const { username, email, password} = req.body
 
@@ -60,6 +64,55 @@ router.post('/signup', (req, res) => {
       res.status(500).json({ message: "Internal Server Error" })
     })
 })
+
+
+// Login Route
  
+router.post('/login', (req, res, next) => {
+    const { email, password } = req.body;
+   
+    // Check if email or password are provided as empty string 
+    if (email === '' || password === '') {
+      res.status(400).json({ message: "Provide email and password." });
+      return;
+    }
+   
+    // Check the users collection if a user with the same email exists
+    User.findOne({ email })
+      .then((foundUser) => {
+      
+        if (!foundUser) {
+          // If the user is not found, send an error response
+          res.status(401).json({ message: "Email not found." })
+          return;
+        }
+   
+        // Compare the provided password with the one saved in the database
+        const passwordCorrect = bcrypt.compareSync(password, foundUser.password);
+   
+        if (passwordCorrect) {
+          // Deconstruct the user object to omit the password
+          const { _id, email, username } = foundUser;
+          
+          // Create an object that will be set as the token payload
+          const payload = { _id, email, username };
+   
+          // Create and sign the token
+          const authToken = jwt.sign( 
+            payload,
+            process.env.TOKEN_SECRET,
+            { algorithm: 'HS256', expiresIn: "720h" }
+          );
+   
+          // Send the token as the response
+          res.status(200).json({ authToken: authToken });
+        }
+        else {
+          res.status(401).json({ message: "Unable to authenticate the user" });
+        }
+   
+      })
+      .catch(err => res.status(500).json({ message: "Internal Server Error" }));
+  });
 
 module.exports = router
