cef0167142027d618665b17f240d88af096eed59
Fer Ortega <fernando.ortega79@gmail.com>
2022-01-21 14:31:53 +0100

    middleware for admin

--- server/middleware/middelwareIsAdmin.js
+++ server/middleware/middelwareIsAdmin.js
@@ -1,10 +1,14 @@
 const User = require("../models/User.model")
+import { AuthContext } from "../../client/src/context/auth.contex";
 
 
 function getAdminLoggedIn (req, res, next) {
 
+const {user} = useContext(AuthContext)
+const isAdministrator = user.isAdmin
+
     if (email === 'admin@admin.com') {
-        User.isAdmin = true
+        isAdministrator = true
         return;
       }
       next()
